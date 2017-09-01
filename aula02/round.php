0b143365619b447358b5109df72868b10c2cf71b
Aluno <aluno@vm-laravel.season.local>
2017-09-01 19:09:03 -0300

    Aula 02 - Correção

--- aula02/round.php
+++ aula02/round.php
@@ -1,10 +1,9 @@
 <?php
 
-$float = round(1.3); // 1
-$float = round(1.3, 1); // 1.3
-$float = round(1.333333, 1); // 1.3
-$float = round(1.353333, 1); // 1.4
-$float = round(1.333333, 2); // 1.33
-$float = round(1.333333, 2, PHP_ROUND_HALF_DOWN); // 1.33
-$float = round(1.333333, 2, PHP_ROUND_HALF_UP); // 1.34
+$float = round(6.5); // 7
+$float = round(6.4); // 6
+$float = round(6.44, 1); // 6.4
+$float = round(6.45, 1); // 6.5
+$float = round(6.45, 1, PHP_ROUND_HALF_DOWN); // 6.4
+
 echo $float;
